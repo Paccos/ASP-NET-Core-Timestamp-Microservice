ff9c864363f7e2e9082173afbaac182c99b40ba2
Patryk Pekala <ppekala@icloud.com>
2021-01-18 14:07:08 +0100

    Add Requirements for valid timestamp strings to tests

--- TimeStampAPITests/TimestampControllerTest.cs
+++ TimeStampAPITests/TimestampControllerTest.cs
@@ -1,14 +1,56 @@
 using System;
 using Xunit;
 
+using TimeStampAPI.Controllers;
 namespace TimeStampAPITests
 {
-    public class UnitTest1
+    public class TimestampControllerTest
     {
-        [Fact]
-        public void Test1()
-        {
+        TimestampController controllerUnderTest;
 
+        [Fact]
+        public void GetWithValidDateStrReturnsValidUnixTime()
+        {
+            controllerUnderTest = new TimestampController();
+
+            var expectedResult = "1451001600000";
+            var result = controllerUnderTest.Get("2015-12-25");
+
+            Assert.Equal(expectedResult, result.Value.Unix);
+        }
+
+        [Fact]
+        public void GetWithValidDateStrReturnsValidDateString()
+        {
+            controllerUnderTest = new TimestampController();
+
+            var expectedResult = "Fri, 25 Dec 2015 00:00:00 GMT";
+            var result = controllerUnderTest.Get("2015-12-25");
+
+            Assert.Equal(expectedResult, result.Value.Utc);
+        }
+
+        [Fact]
+        public void GetWithValidUnixStrReturnsValidUnixTime()
+        {
+            controllerUnderTest = new TimestampController();
+
+            var expectedResult = "1451001600000";
+            var result = controllerUnderTest.Get("1451001600000");
+
+            Assert.Equal(expectedResult, result.Value.Unix);
+        }
+
+        [Fact]
+        public void GetWithValidUnixStrReturnsValidDateString()
+        {
+            controllerUnderTest = new TimestampController();
+
+            var expectedResult = "Fri, 25 Dec 2015 00:00:00 GMT";
+            var result = controllerUnderTest.Get("1451001600000");
+
+            Assert.Equal(expectedResult, result.Value.Utc);
+        }
         }
     }
 }
