2be82554e3270b3b2d1ca466f77cd0fab0f820d2
Patryk Pekala <ppekala@icloud.com>
2021-01-18 14:11:35 +0100

    Return Timestamp on Get

--- TimeStampAPI/Controllers/TimestampController.cs
+++ TimeStampAPI/Controllers/TimestampController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TimeStampAPI.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,36 +12,8 @@ namespace TimeStampAPI.Controllers
     [Route("api/[controller]")]
     public class TimestampController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        // GET api/timestamp/{date_str-or-unix_epoch}
+        [HttpGet("{dateStr}")]
+        public ActionResult<Timestamp> Get(string dateStr) => new Timestamp(dateStr);
     }
 }
