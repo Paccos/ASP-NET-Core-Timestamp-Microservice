0efc200e806424195c344775e5d4897df88a5858
Patryk Pekala <ppekala@icloud.com>
2021-01-18 14:09:36 +0100

    Add Timestamp model with parsing for date strings and unix epochs

--- TimeStampAPI/Model/Timestamp.cs
+++ TimeStampAPI/Model/Timestamp.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.Linq;
+using System.Text.Json.Serialization;
+
 namespace TimeStampAPI.Model
 {
     public class Timestamp
     {
-        public Timestamp()
+        [JsonIgnore]
+        public DateTime Date { get; set; }
+
+        public string Utc => Date.ToString("ddd, dd MMM yyyy HH:mm:ss") + " GMT";
+        public string Unix
         {
+            get
+            {
+                var offset = new DateTimeOffset(Date);
+                return offset.ToUnixTimeMilliseconds()
+                             .ToString();
+            }
+        }
+
+
+        public Timestamp(string dateStr)
+        {
+            var dateComponents = dateStr.Split("-");
+
+            if (dateComponents.Length != 3)
+            {
+                try
+                {
+                    var unixMilliseconds = long.Parse(dateStr);
+                    var offset = DateTimeOffset.FromUnixTimeMilliseconds(unixMilliseconds);
+
+                    Date = offset.UtcDateTime;
+                }
+                catch
+                {
+                    throw new ArgumentException("dateStr has wrong format");
+                }
+            }
+            else
+            {
+                try
+                {
+                    var ymd = dateComponents.Select(c => int.Parse(c)).ToArray();
+
+                    Date = new DateTime(ymd[0], ymd[1], ymd[2], 0, 0, 0, DateTimeKind.Utc);
+                }
+                catch
+                {
+                    throw new ArgumentException("dateStr has wrong format");
+                }
+            }
         }
     }
 }
